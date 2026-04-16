e9a0be0c8068d1f249186185476d362ba2c09841
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-16 11:57:55 +0500

    add EIP and NAT Gateway resources

---
+++ environment/nat.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "nat" {
+  for_each = local.active_nats
+  domain   = "vpc"
+  tags     = { Name = "${local.project_name}-nat-eip-${each.key}" }
+}
+
+resource "aws_nat_gateway" "main" {
+  for_each      = local.active_nats
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = each.value.public_subnet_id
+
+  # NAT64 is essential for IPv6-native subnets to talk to IPv4 internet
+  connectivity_type = "public"
+
+  tags = { Name = "${local.project_name}-nat-gw-${each.key}" }
+}
