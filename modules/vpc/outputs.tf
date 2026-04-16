58e7ff3718c38b8f90d2e1ae393636f215980c5d
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-16 21:47:28 +0500

    add outputs

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,61 @@
+output "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  value       = aws_vpc.this.id
+}
+
+# ------------- IPv6 Only Subnets -------------
+output "public_ipv4_subnets" {
+  description = "Map of public subnet IDs"
+  value = {
+    "public_ipv4_a" = aws_subnet.ipv4_public_a.id
+    "public_ipv4_b" = aws_subnet.ipv4_public_b.id
+    "public_ipv4_c" = aws_subnet.ipv4_public_c.id
+  }
+}
+
+output "private_ipv4_subnets" {
+  description = "Map of private subnet IDs"
+  value = {
+    "public_ipv4_a" = aws_subnet.ipv4_private_a.id
+    "public_ipv4_b" = aws_subnet.ipv4_private_b.id
+    "public_ipv4_c" = aws_subnet.ipv4_private_c.id
+  }
+}
+
+# ------------- IPv6 Only Subnets -------------
+output "public_ipv6_subnets" {
+  description = "Map of public subnet IDs"
+  value = {
+    "public_ipv6_a" = aws_subnet.ipv6_public_a.id
+    "public_ipv6_b" = aws_subnet.ipv6_public_b.id
+    "public_ipv6_c" = aws_subnet.ipv6_public_c.id
+  }
+}
+
+output "private_ipv6_subnets" {
+  description = "Map of private subnet IDs"
+  value = {
+    "public_ipv6_a" = aws_subnet.ipv6_private_a.id
+    "public_ipv6_b" = aws_subnet.ipv6_private_b.id
+    "public_ipv6_c" = aws_subnet.ipv6_private_c.id
+  }
+}
+
+# ------------- Dual-Stack Subnets -------------
+output "public_dual_stack_subnets" {
+  description = "Map of public subnet IDs"
+  value = {
+    "public_dual_stack_a" = aws_subnet.dual_stack_public_a.id
+    "public_dual_stack_b" = aws_subnet.dual_stack_public_b.id
+    "public_dual_stack_c" = aws_subnet.dual_stack_public_c.id
+  }
+}
+
+output "private_dual_stack_subnets" {
+  description = "Map of private subnet IDs"
+  value = {
+    "public_dual_stack_a" = aws_subnet.dual_stack_private_a.id
+    "public_dual_stack_b" = aws_subnet.dual_stack_private_b.id
+    "public_dual_stack_c" = aws_subnet.dual_stack_private_c.id
+  }
+}
