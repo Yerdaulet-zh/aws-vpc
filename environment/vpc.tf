7b765533053a3e805ea0430f01f87fc5c0d87a23
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-16 09:57:40 +0500

    use amazon provided IPv6 GUA

--- environment/vpc.tf
+++ environment/vpc.tf
@@ -2,14 +2,16 @@ resource "aws_vpc" "this" {
   ipv4_ipam_pool_id   = data.aws_vpc_ipam_pool.dev_ipv4.id
   ipv4_netmask_length = 20
 
-  ipv6_ipam_pool_id   = data.aws_vpc_ipam_pool.dev_ipv6.id
-  ipv6_netmask_length = 56
+  # ipv6_ipam_pool_id   = aws_vpc_ipv6_cidr_block_association.example
+  # ipv6_netmask_length = 56
+
+  # assign Amazon provided IPv6 pool of IP addresses which GUA
+  assign_generated_ipv6_cidr_block = true
 
   instance_tenancy                     = "default"
   enable_dns_support                   = true
   enable_network_address_usage_metrics = false
   enable_dns_hostnames                 = true
-  #   assign_generated_ipv6_cidr_block     = false
 
   tags = {
     Name        = "Development VPC for EKS"
