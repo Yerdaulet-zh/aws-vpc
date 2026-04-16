5405bf983bd6a1003c6c18d057f308900dfd7065
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-16 13:10:15 +0500

    change s3 bycket key

--- environment/prod/backend.tf
+++ environment/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "epam-terrform-state-bucket"
-    key          = "vpc/eks/dual-stack/prod/terraform.tfstate"
+    key          = "895587011312_NetworkAdministrator/vpc/dual-stack/eks/prod/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
     use_lockfile = true
--- environment/test/backend.tf
+++ environment/test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "epam-terrform-state-bucket"
-    key          = "vpc/eks/dual-stack/test/terraform.tfstate"
+    key          = "895587011312_NetworkAdministrator/vpc/dual-stack/eks/test/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
     use_lockfile = true
