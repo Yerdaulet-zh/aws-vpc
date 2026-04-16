431144f71326d0792b425676704c8fc00f006d77
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-16 10:23:16 +0500

    add acl rules for ipv6

--- environment/acl.tf
+++ environment/acl.tf
@@ -5,6 +5,7 @@ resource "aws_network_acl" "main" {
   }
 }
 
+# ACL Rules for IPv4
 resource "aws_network_acl_rule" "acl_rule_ingress" {
   for_each = {
     "100" = "tcp"
@@ -35,4 +36,37 @@ resource "aws_network_acl_rule" "acl_rule_egress" {
   rule_action    = "allow"
   from_port      = 0
   to_port        = 65535
+}
+
+# ACL Rules for IPv6
+resource "aws_network_acl_rule" "acl_rule_ingress_v6" {
+  for_each = {
+    "200" = "tcp"
+    "210" = "udp"
+  }
+
+  network_acl_id  = aws_network_acl.main.id
+  rule_number     = each.key
+  egress          = false
+  protocol        = each.value
+  ipv6_cidr_block = "::/0"
+  rule_action     = "allow"
+  from_port       = 0
+  to_port         = 65535
+}
+
+resource "aws_network_acl_rule" "acl_rule_egress_v6" {
+  for_each = {
+    "200" = "tcp"
+    "210" = "udp"
+  }
+
+  network_acl_id  = aws_network_acl.main.id
+  rule_number     = each.key
+  egress          = true
+  protocol        = each.value
+  ipv6_cidr_block = "::/0"
+  rule_action     = "allow"
+  from_port       = 0
+  to_port         = 65535
 }
