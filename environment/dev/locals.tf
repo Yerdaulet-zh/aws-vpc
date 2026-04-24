1d40e0f9224c7b5216ff5515086c84e39919385e
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-24 19:21:47 +0500

    update cluster name

--- environment/dev/locals.tf
+++ environment/dev/locals.tf
@@ -2,5 +2,5 @@ locals {
   region           = "eu-central-1"
   environment      = "Development"
   project_name     = "EKS-Academy"
-  eks_cluster_name = "${local.environment}_${local.project_name}"
+  eks_cluster_name = "eks-academy-dev"
 }
