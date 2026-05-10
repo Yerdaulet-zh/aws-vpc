b2c0bae435fa74e33deb8cf9e822548adbdf3442
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-05-10 14:22:08 +0500

    bug-fix: route all public subnets to IGW

--- modules/vpc/route_tables_public_associations.tf
+++ modules/vpc/route_tables_public_associations.tf
@@ -1,12 +1,11 @@
 resource "aws_route_table_association" "public" {
-  # Filter the subnets map to only include subnets where public IP is enabled
+  # We filter the subnets map to include only the subnets that should be public.
+  # Based on your variable, these are subnets where Name contains 'Public'.
   for_each = {
     for k, v in var.subnets : k => v
-    if v.map_public_ip_on_launch == true
+    if length(regexall("public", lower(k))) > 0
   }
 
-  # Reference the dynamically created subnet ID
-  subnet_id = aws_subnet.subnets[each.key].id
-
+  subnet_id      = aws_subnet.subnets[each.key].id
   route_table_id = aws_route_table.dual_stack_public_route_table.id
 }
